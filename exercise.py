c2a0928bbdf024a96eea0c008756de95df67b2c1
mosman7 <mohamedosman998@hotmail.com>
2022-10-13 10:08:07 +0100

    menu

--- exercise.py
+++ exercise.py
@@ -1,9 +1,9 @@
-for num in range(1,101):        #goes through numbers in specified range
-    if(num%3==0 and num%5==0):    #if number is divisible by both 3 and 5
+for x in range(1,101):        #goes through numbers in specified range
+    if(x%3==0 and x%5==0):    #if number is divisible by both 3 and 5
       print("FizzBuzz")
-    elif(num%3 == 0):             #if number is divisible by 3
+    elif(x%3 == 0):             #if number is divisible by 3
       print("Fizz")
-    elif(num%5 == 0):             #if number is divisible by 5
+    elif(x%5 == 0):             #if number is divisible by 5
       print("Buzz")
     else:                         #if none of the conditions are satisfied, print number
-      print(num)
+      print(x)
